acb06ae0baf56d1a5e13d1a60727da372218dfd0
Praveen Kumar <praveenscience@gmail.com>
2020-04-06 00:18:32 +0100

    Updates

---
+++ precache-manifest.18f56a6a27fc318681188c602962f925.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6014229e02e4d3a03f54dfb77361262",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82f8765d8c3d07812eb8",
+    "url": "/static/css/main.4a86210d.chunk.css"
+  },
+  {
+    "revision": "9a1d9541decb711704af",
+    "url": "/static/js/2.4298aae2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4298aae2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82f8765d8c3d07812eb8",
+    "url": "/static/js/main.8e805b68.chunk.js"
+  },
+  {
+    "revision": "4b0b8649b9ba65f2c80b",
+    "url": "/static/js/runtime-main.966c5448.js"
+  },
+  {
+    "revision": "e6a3bc3b75cd7c554b8290ca3962bceb",
+    "url": "/static/media/apple.e6a3bc3b.svg"
+  },
+  {
+    "revision": "a187ef6388db0ba675988d15828957be",
+    "url": "/static/media/backedby.a187ef63.png"
+  },
+  {
+    "revision": "dd9ecbace0d96350360010b177d23e14",
+    "url": "/static/media/backedinstitutes.dd9ecbac.png"
+  },
+  {
+    "revision": "fc1bb3de97fd40efd3885eda298bebc8",
+    "url": "/static/media/child-expert.fc1bb3de.svg"
+  },
+  {
+    "revision": "ee748501312f55fd7e522497d2e92db8",
+    "url": "/static/media/footer-logo.ee748501.png"
+  },
+  {
+    "revision": "91188fe36ad167b45811009a15b9204e",
+    "url": "/static/media/google.91188fe3.svg"
+  },
+  {
+    "revision": "2cfa3b950d0de63aa7d7f81f0afce6a5",
+    "url": "/static/media/home-banner-balloon.2cfa3b95.png"
+  },
+  {
+    "revision": "d10ed3104fafa12de90a61049dd6f9af",
+    "url": "/static/media/home-banner-mobile.d10ed310.png"
+  },
+  {
+    "revision": "6533af263260074330e33c150d0e48e6",
+    "url": "/static/media/logo.6533af26.png"
+  }
+]);
