b52e43ea97cac99ddd93fe5b77e95409c5c9805d
Praveen Kumar <praveenscience@gmail.com>
2020-04-05 17:26:10 +0100

    Implemented CSS hamburger menu for mobiles.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Brand from "./_Brand";
 import PrimaryNav from "./_PrimaryNav";
+import Ham from "./_Ham";
 
 const Header = () => {
   return (
     <div className="Header">
+      <Ham />
       <Brand />
       <PrimaryNav />
     </div>
---
+++ src/components/Header/_Ham.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Ham = () => {
+  return (
+    <div className="Header-Ham">
+      <span />
+      <span />
+      <span />
+    </div>
+  );
+};
+
+export default Ham;
