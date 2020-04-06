3f434af96f228609f6272981e88a6caca43b57c8
Praveen Kumar <praveenscience@gmail.com>
2020-04-06 22:25:33 +0100

    Removed console logs.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,7 +2,6 @@ import React from "react";
 
 const About = () => {
   React.useEffect(() => {
-    console.log("Loaded");
     window.scrollTo(0, 0);
   }, []);
   return <main className="About"></main>;
