c4d22f28a7e1a6436b6b31d0ccd06c46422f8ec8
Praveen Kumar <praveenscience@gmail.com>
2020-04-06 21:07:21 +0100

    Implmented Browser Routing.

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,25 @@ import React from "react";
 import Header from "./Shared/Header/Header";
 import Footer from "./Shared/Footer/Footer";
 import Home from "./Home/Home";
+import { Switch, Route } from "react-router-dom";
+import Terms from "./InfoPages/Terms";
+import Privacy from "./InfoPages/Privacy";
 
 const App = () => {
   return (
     <div className="App">
       <Header />
-      <Home />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/terms">
+          <Terms />
+        </Route>
+        <Route path="/privacy">
+          <Privacy />
+        </Route>
+      </Switch>
       <Footer />
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 
@@ -7,7 +8,9 @@ import "./styles/App.scss";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
