3559efff9aef2e7724089e6c378c85d74e920fd8
Praveen Kumar <praveenscience@gmail.com>
2020-04-06 21:00:47 +0100

    Implemented Privacy and Terms pages.

---
+++ src/components/InfoPages/Terms.js
@@ -0,0 +1,94 @@
+import React from "react";
+import InfoPage from "../Shared/InfoPage/InfoPage";
+
+const Terms = () => {
+  return (
+    <InfoPage Heading="Terms">
+      <h2>Heading 2</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
+        exercitationem quae perferendis magni laudantium provident consequuntur
+        voluptates! Perspiciatis eligendi quaerat eum, aliquam voluptatibus,
+        distinctio ipsum sequi officiis fuga cupiditate veritatis.
+      </p>
+      <p>
+        Maxime consectetur, mollitia error eius non quo nemo nulla explicabo
+        voluptatibus! Excepturi sapiente obcaecati impedit facere molestias quam
+        nam nisi itaque magni tempore reiciendis sed assumenda, recusandae et
+        repellat qui.
+      </p>
+      <ul>
+        <li>List Item 1</li>
+        <li>List Item 2</li>
+        <li>List Item 3</li>
+      </ul>
+      <h3>Heading 3</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
+        exercitationem quae perferendis magni laudantium provident consequuntur
+        voluptates! Perspiciatis eligendi quaerat eum, aliquam voluptatibus,
+        distinctio ipsum sequi officiis fuga cupiditate veritatis.
+      </p>
+      <p>
+        Maxime consectetur, mollitia error eius non quo nemo nulla explicabo
+        voluptatibus! Excepturi sapiente obcaecati impedit facere molestias quam
+        nam nisi itaque magni tempore reiciendis sed assumenda, recusandae et
+        repellat qui.
+      </p>
+      <h3>Heading 3</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
+        exercitationem quae perferendis magni laudantium provident consequuntur
+        voluptates! Perspiciatis eligendi quaerat eum, aliquam voluptatibus,
+        distinctio ipsum sequi officiis fuga cupiditate veritatis.
+      </p>
+      <p>
+        Maxime consectetur, mollitia error eius non quo nemo nulla explicabo
+        voluptatibus! Excepturi sapiente obcaecati impedit facere molestias quam
+        nam nisi itaque magni tempore reiciendis sed assumenda, recusandae et
+        repellat qui.
+      </p>
+      <h3>Heading 3</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
+        exercitationem quae perferendis magni laudantium provident consequuntur
+        voluptates! Perspiciatis eligendi quaerat eum, aliquam voluptatibus,
+        distinctio ipsum sequi officiis fuga cupiditate veritatis.
+      </p>
+      <p>
+        Maxime consectetur, mollitia error eius non quo nemo nulla explicabo
+        voluptatibus! Excepturi sapiente obcaecati impedit facere molestias quam
+        nam nisi itaque magni tempore reiciendis sed assumenda, recusandae et
+        repellat qui.
+      </p>
+      <h2>Heading 2</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
+        exercitationem quae perferendis magni laudantium provident consequuntur
+        voluptates! Perspiciatis eligendi quaerat eum, aliquam voluptatibus,
+        distinctio ipsum sequi officiis fuga cupiditate veritatis.
+      </p>
+      <p>
+        Maxime consectetur, mollitia error eius non quo nemo nulla explicabo
+        voluptatibus! Excepturi sapiente obcaecati impedit facere molestias quam
+        nam nisi itaque magni tempore reiciendis sed assumenda, recusandae et
+        repellat qui.
+      </p>
+      <h2>Heading 2</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
+        exercitationem quae perferendis magni laudantium provident consequuntur
+        voluptates! Perspiciatis eligendi quaerat eum, aliquam voluptatibus,
+        distinctio ipsum sequi officiis fuga cupiditate veritatis.
+      </p>
+      <p>
+        Maxime consectetur, mollitia error eius non quo nemo nulla explicabo
+        voluptatibus! Excepturi sapiente obcaecati impedit facere molestias quam
+        nam nisi itaque magni tempore reiciendis sed assumenda, recusandae et
+        repellat qui.
+      </p>
+    </InfoPage>
+  );
+};
+
+export default Terms;
