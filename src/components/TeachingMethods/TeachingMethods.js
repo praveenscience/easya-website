3f07c54c9b8038b27b604958ecc57cf012e50666
Praveen Kumar <praveenscience@gmail.com>
2020-04-08 21:26:50 +0100

    Done with Heading in Teaching Methods.

--- src/components/TeachingMethods/TeachingMethods.js
+++ src/components/TeachingMethods/TeachingMethods.js
@@ -4,7 +4,11 @@ const TeachingMethods = () => {
   React.useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
-  return <main className="TeachingMethods"></main>;
+  return (
+    <main className="TeachingMethods">
+      <h1>EasyA's revolutionary approach to learning</h1>
+    </main>
+  );
 };
 
 export default TeachingMethods;
