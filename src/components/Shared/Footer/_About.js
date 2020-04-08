2a571f972f6ad016f150c071a5afab3f8282450b
Praveen Kumar <praveenscience@gmail.com>
2020-04-08 10:13:05 +0100

    Updated footer to use the right logo.

--- src/components/Shared/Footer/_About.js
+++ src/components/Shared/Footer/_About.js
@@ -1,18 +1,12 @@
 import React from "react";
 import FooterAppleLogo from "../../../assets/apple.svg";
 import FooterGoogleLogo from "../../../assets/google.svg";
-import FooterLogo from "../../../assets/footer-logo.png";
 import Logo from "../../../assets/logo.png";
 
 const FooterAbout = () => {
   return (
     <section className="Footer-About">
-      <img
-        src={FooterLogo}
-        className="Footer-About-Desktop"
-        alt="Footer Logo"
-      />
-      <img src={Logo} className="Footer-About-Mobile" alt="Footer Logo" />
+      <img src={Logo} className="Footer-About-Logo" alt="Footer Logo" />
       <p>
         The EasyA Math Tutoring App is built for students on their platform of
         choice - smartphones!
