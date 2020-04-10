fe47565369b9fae50232b116a91c325042e2236d
Praveen Kumar <praveenscience@gmail.com>
2020-04-10 18:55:31 +0100

    Completed Board in WMCAL.

--- src/components/WMCAL/_WMCALBoard.js
+++ src/components/WMCAL/_WMCALBoard.js
@@ -1,7 +1,19 @@
 import React from "react";
+import FeaturedTutorsImg from "../../assets/featuredtutorsimg.png";
 
 const WMCALBoard = () => {
-  return <section className="WMCAL-Board"></section>;
+  return (
+    <section className="WMCAL-Board">
+      <img src={FeaturedTutorsImg} alt="Featured Tutors" />
+      <h2>Academic Advisory Board</h2>
+      <div className="WMCAL-Board-Slider easya-bgc-seance">
+        Slider will be done once the website is converted into
+        <br />
+        static HTML &amp; CSS.
+      </div>
+      <span className="RoundIcon RoundIconRight" />
+    </section>
+  );
 };
 
 export default WMCALBoard;
