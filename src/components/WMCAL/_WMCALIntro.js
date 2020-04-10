4fb4c9b1251f939c95cf1974f2861ebcdf45248f
Praveen Kumar <praveenscience@gmail.com>
2020-04-10 15:02:31 +0100

    WMCAL Intro section completed for desktop.

--- src/components/WMCAL/_WMCALIntro.js
+++ src/components/WMCAL/_WMCALIntro.js
@@ -1,7 +1,31 @@
 import React from "react";
 
 const WMCALIntro = () => {
-  return <section className="WMCAL-Intro"></section>;
+  return (
+    <section className="WMCAL-Intro">
+      <div className="WMCAL-Intro-Text">
+        <h2>
+          Our mission is to provide the technology and experts to help every
+          student thrive
+        </h2>
+        <p className="author">
+          <strong>Christina Pierce -</strong>
+          Easy Education Advisory Board
+        </p>
+        <p>
+          Curabitur lobortis id lorem id bibendum. Ut id consectetur magna.
+          Quisque volutpat augue enim, pulvinar lobortis nibh lacinia at.
+          Vestibulum nec erat ut mi sollicitudin porttitor id sit amet risus.
+          Nam tempus vel odio vitae aliquam. In imperdiet eros id lacus
+          vestibulum vestibulum. Suspendisse fermentum sem sagittis ante
+          venenatis egestas quis vel justo. Maecenas semper suscipit nunc.
+        </p>
+      </div>
+      <div className="WMCAL-Intro-Image">
+        <img src="https://i.imgur.com/4lt2Ckj.png" alt="Christina Pierce" />
+      </div>
+    </section>
+  );
 };
 
 export default WMCALIntro;
