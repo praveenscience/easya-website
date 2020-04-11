f17b1b51d5870c4f499e1d0b0638884e06ddb6dc
Praveen Kumar <praveenscience@gmail.com>
2020-04-11 19:39:02 +0100

    Still wondering section in testimonials.

--- src/components/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials.js
@@ -1,6 +1,9 @@
 import React from "react";
 import TestIntro from "./_TestIntro";
 import TMReviews from "../TeachingMethods/_TMReviews";
+import Paths from "../../constants/Paths";
+import AboutCurious from "../About/_AboutCurious";
+import WhizImg from "../../assets/achievements/whiz.svg";
 
 const Testimonials = () => {
   React.useEffect(() => {
@@ -10,6 +13,12 @@ const Testimonials = () => {
     <main className="Testimonials">
       <TestIntro />
       <TMReviews />
+      <AboutCurious
+        Image={WhizImg}
+        Title="Still wondering if EasyA is right for your family?"
+        Action="How we teach"
+        ActionLink={Paths.TeachingMethods}
+      />
     </main>
   );
 };
