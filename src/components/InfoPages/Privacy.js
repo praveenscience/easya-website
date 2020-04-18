42c79088f17dda51a8a735023846fecef51d1716
Praveen Kumar <praveenscience@gmail.com>
2020-04-18 21:27:01 +0100

    Contact privacy and responsiveness on WMCAL. (#4)
    * Privacy Policy done
    * Contact Us responsiveness
    * Contact form fixes.
    * Responsive fixes added.
    
    Co-authored-by: nikhar444 <nikhar.jain11@gmail.com>

--- src/components/InfoPages/Privacy.js
+++ src/components/InfoPages/Privacy.js
@@ -6,89 +6,188 @@ const Privacy = () => {
     window.scrollTo(0, 0);
   }, []);
   return (
-    <InfoPage Heading="Privacy">
-      <h2>Heading 2</h2>
+    <InfoPage Heading="Privacy Policy">
+      <h2>Introduction</h2>
+
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
-        exercitationem quae perferendis magni laudantium provident consequuntur
-        voluptates! Perspiciatis eligendi quaerat eum, aliquam voluptatibus,
-        distinctio ipsum sequi officiis fuga cupiditate veritatis.
+        This policy (together with our Terms & Conditions) applies to your use
+        of:
+        <br />• The EasyA mobile app (App) and the EasyA website (Website),
+        together Our Services; and
+        <br />• Any of the services accessible through the App (Services) that
+        are available on the Website or other sites of ours (Services Sites).
+        This policy sets out the basis on which any personal data we collect
+        from you, or that you provide to us, will be processed by us.
+        <br />
+        <br />
+        EasyA Ltd is the controller and is responsible for your personal data
+        (collectively referred to as EasyA, We, Us or Our in this policy).
       </p>
+
+      <h2>Data we collect</h2>
+
       <p>
-        Maxime consectetur, mollitia error eius non quo nemo nulla explicabo
-        voluptatibus! Excepturi sapiente obcaecati impedit facere molestias quam
-        nam nisi itaque magni tempore reiciendis sed assumenda, recusandae et
-        repellat qui.
+        We may collect, use, store and transfer different kinds of personal data
+        about you as follows:
+        <br />• Identity Data.
+        <br />• Contact Data.
+        <br />• Device Data.
+        <br />• Content Data.
+        <br />• Profile Data.
+        <br />• Usage Data.
+        <br />• Marketing and Communications Data.
+        <br />• Location Data
+        <br />
+        We also collect, use and share Aggregated Data such as statistical or
+        demographic data for any purpose. Aggregated Data could be derived from
+        your personal data but is not considered personal data in law as this
+        data will not directly or indirectly reveal your identity. For example,
+        we may aggregate your Usage Data to calculate the percentage of users
+        accessing a specific App feature. However, if we combine or connect
+        Aggregated Data with your personal data so that it can directly or
+        indirectly identify you, we treat the combined data as personal data
+        which will be used in accordance with this privacy policy.
+        <br />
+        We do not collect any Special Categories of Personal Data about you
+        (this includes details about your race or ethnicity, religious or
+        philosophical beliefs, sex life, sexual orientation, political opinions,
+        trade union membership, information about your health, and genetic and
+        biometric data). Nor do we collect any information about criminal
+        convictions and offences.
       </p>
-      <ul>
-        <li>List Item 1</li>
-        <li>List Item 2</li>
-        <li>List Item 3</li>
-      </ul>
-      <h3>Heading 3</h3>
+
+      <h2>How we collect data</h2>
+
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
-        exercitationem quae perferendis magni laudantium provident consequuntur
-        voluptates! Perspiciatis eligendi quaerat eum, aliquam voluptatibus,
-        distinctio ipsum sequi officiis fuga cupiditate veritatis.
+        We will collect and process the following data about you:
+        <br />
+        <br />• Information you give us. This is information (including
+        Identity, Contact, and Marketing and Communications Data) you consent to
+        giving us about you by filling in forms on the App and the Services
+        Sites (together Our Sites), or by corresponding with us (for example, by
+        email or chat). It includes information you provide when you register to
+        use the App, subscribe to any of our Services, search for an App or
+        Service, make an in-App purchase, or enter a competition, promotion or
+        survey, and when you report a problem with an App, our Services, or any
+        of Our Sites. If you contact us, we will keep a record of that
+        correspondence.
+        <br />
+        <br />• Information we collect about you and your device. Each time you
+        visit one of Our Sites or use one of our Apps we will automatically
+        collect personal data including Device, Content and Usage Data. We
+        collect this data using cookies and other similar technologies.
+        <br />
+        <br />• Information we receive from other sources including third
+        parties and publicly available sources. We will receive personal data
+        about you from various third parties. For example, analytics providers
+        such as Google based outside the EU;
+        <br />
+        <br />• Unique application numbers. When you want to install or
+        uninstall a Service containing a unique application number or when such
+        a Service searches for automatic updates, that number and information
+        about your installation, for example, the type of operating system, may
+        be sent to us.
       </p>
+
+      <h2>Cookies</h2>
       <p>
-        Maxime consectetur, mollitia error eius non quo nemo nulla explicabo
-        voluptatibus! Excepturi sapiente obcaecati impedit facere molestias quam
-        nam nisi itaque magni tempore reiciendis sed assumenda, recusandae et
-        repellat qui.
+        We use cookies to distinguish you from other users of the App, Website,
+        the distribution platform (Appstore) or Services Sites and to remember
+        your preferences. This helps us to provide you with a good experience
+        when you use the App or browse any of Our Sites and also allows us to
+        improve the App and Our Sites.
       </p>
-      <h3>Heading 3</h3>
+
+      <h2>How we use your data</h2>
+
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
-        exercitationem quae perferendis magni laudantium provident consequuntur
-        voluptates! Perspiciatis eligendi quaerat eum, aliquam voluptatibus,
-        distinctio ipsum sequi officiis fuga cupiditate veritatis.
+        We will only use your personal data when the law allows us to do so.
+        Most commonly we will use your personal data in the following
+        circumstances:
+        <br />• Where you have consented before the processing.
+        <br />• Where we need to perform a contract we are about to enter or
+        have entered with you.
+        <br />• Where it is necessary for our legitimate interests (or those of
+        a third party) and your interests and fundamental rights do not override
+        those interests.
+        <br />• Where we need to comply with a legal or regulatory obligation.
+        <br />
+        We will only send you direct marketing communications by email or text
+        if we have your consent. You have the right to withdraw that consent at
+        any time by contacting us.
       </p>
+
+      <h2>International transfers</h2>
+
       <p>
-        Maxime consectetur, mollitia error eius non quo nemo nulla explicabo
-        voluptatibus! Excepturi sapiente obcaecati impedit facere molestias quam
-        nam nisi itaque magni tempore reiciendis sed assumenda, recusandae et
-        repellat qui.
+        We ensure your personal data is protected by requiring all our group
+        companies to follow the same rules when processing your personal data.
+        These rules are called “binding corporate rules”.
+        <br />
+        Some of our external third parties are based outside the EEA so their
+        processing of your personal data will involve a transfer of data outside
+        the EEA.
+        <br />
+        Whenever we transfer your personal data out of the EEA, we ensure a
+        similar degree of protection is afforded to it by ensuring at least one
+        of the following safeguards is implemented:
+        <br />• We will only transfer your personal data to countries that have
+        been deemed to provide an adequate level of protection for personal data
+        by the European Commission.
+        <br />• Where we use certain service providers, we may use specific
+        contracts approved by the European Commission which give personal data
+        the same protection it has in Europe.
+        <br />• Where we use providers based in the US, we may transfer data to
+        them if they are part of the Privacy Shield which requires them to
+        provide similar protection to personal data shared between Europe and
+        the US.
       </p>
-      <h3>Heading 3</h3>
+
+      <h2>Data Security</h2>
+
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
-        exercitationem quae perferendis magni laudantium provident consequuntur
-        voluptates! Perspiciatis eligendi quaerat eum, aliquam voluptatibus,
-        distinctio ipsum sequi officiis fuga cupiditate veritatis.
+        All information you provide to us is stored on our secure servers. Any
+        payment transactions carried out by us or our chosen third-party
+        provider of payment processing services will be encrypted. Where we have
+        given you (or where you have chosen) a password that enables you to
+        access certain parts of Our Sites, you are responsible for keeping this
+        password confidential. We ask you not to share a password with anyone.
+        <br />
+        Once we have received your information, we will use strict procedures
+        and security features to try to prevent your personal data from being
+        accidentally lost, used or accessed in an unauthorised way.
+        <br />
+        We will collect and store personal data on your Device using application
+        data caches.
       </p>
+
+      <h2>Data Retention</h2>
       <p>
-        Maxime consectetur, mollitia error eius non quo nemo nulla explicabo
-        voluptatibus! Excepturi sapiente obcaecati impedit facere molestias quam
-        nam nisi itaque magni tempore reiciendis sed assumenda, recusandae et
-        repellat qui.
+        In some circumstances you can ask us to delete your data, such as where
+        we have no good reason to retain it any longer.
+        <br />
+        In some circumstances we will anonymise your personal data (so that it
+        can no longer be associated with you) for research or statistical
+        purposes, in which case we may use this information indefinitely without
+        further notice to you.
       </p>
-      <h2>Heading 2</h2>
+
+      <h2>Choice of law</h2>
+
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
-        exercitationem quae perferendis magni laudantium provident consequuntur
-        voluptates! Perspiciatis eligendi quaerat eum, aliquam voluptatibus,
-        distinctio ipsum sequi officiis fuga cupiditate veritatis.
+        This privacy policy, its subject matter and its formation, is governed
+        by English law. You and we both agree that the courts of England and
+        Wales will have exclusive jurisdiction.
       </p>
+
+      <h2>Changes to Privacy Policy</h2>
       <p>
-        Maxime consectetur, mollitia error eius non quo nemo nulla explicabo
-        voluptatibus! Excepturi sapiente obcaecati impedit facere molestias quam
-        nam nisi itaque magni tempore reiciendis sed assumenda, recusandae et
-        repellat qui.
-      </p>
-      <h2>Heading 2</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
-        exercitationem quae perferendis magni laudantium provident consequuntur
-        voluptates! Perspiciatis eligendi quaerat eum, aliquam voluptatibus,
-        distinctio ipsum sequi officiis fuga cupiditate veritatis.
-      </p>
-      <p>
-        Maxime consectetur, mollitia error eius non quo nemo nulla explicabo
-        voluptatibus! Excepturi sapiente obcaecati impedit facere molestias quam
-        nam nisi itaque magni tempore reiciendis sed assumenda, recusandae et
-        repellat qui.
+        We keep our privacy policy under regular review.
+        <br />
+        This version was last updated on 24 September 2019. It may change and if
+        it does, these changes will be posted on this page. The new policy may
+        be displayed on-screen and you may be required to read and accept the
+        changes to continue your use of the App or the Services.
       </p>
     </InfoPage>
   );
