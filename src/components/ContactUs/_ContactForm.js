b3265667aad064f530e01f67899b67a815bdcfb7
Praveen Kumar <praveenscience@gmail.com>
2020-04-21 16:25:21 +0100

    Prettier fix.

--- src/components/ContactUs/_ContactForm.js
+++ src/components/ContactUs/_ContactForm.js
@@ -5,24 +5,24 @@ const FormElements = [
   {
     Type: "text",
     Label: "Your Full Name",
-    Name: "full-name",
+    Name: "full-name"
   },
   {
     Type: "email",
     Label: "Your Email Address",
-    Name: "email",
+    Name: "email"
   },
   {
     Type: "select",
     Label: "Admission",
     Name: "admission",
-    Options: ["Student", "Parent"],
+    Options: ["Student", "Parent"]
   },
   {
     Type: "textarea",
     Label: "Your Comment",
-    Name: "comment",
-  },
+    Name: "comment"
+  }
 ];
 const ContactForm = ({ action }) => {
   return (
