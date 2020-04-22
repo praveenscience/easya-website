15ca760043e603a4909e97388a5c16886b43c9eb
Praveen Kumar <praveenscience@gmail.com>
2020-04-22 15:56:20 +0100

    #21: “Listen to them, not us” box arrows added (Become Tutor).

--- src/components/ATBT/_ATBTListen.js
+++ src/components/ATBT/_ATBTListen.js
@@ -1,12 +1,15 @@
 import React from "react";
 import TeacherSlider from "../Shared/Banners/TeacherSlider";
 
+const SliderSettings = {
+  arrows: true
+};
 const ATBTListen = () => {
   return (
     <section className="ATBT-Listen easya-bgc-trans-seance">
       <h2>Listen to them, not us</h2>
       <div className="ATBT-Listen-Slides">
-        <TeacherSlider />
+        <TeacherSlider ExtraSettings={SliderSettings} />
       </div>
     </section>
   );
