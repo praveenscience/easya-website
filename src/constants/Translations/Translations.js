af675cf8357f08fd439b4dd270a75699023fab53
Praveen Kumar <praveenscience@gmail.com>
2020-04-23 12:09:26 +0100

    Implemented Translations.

---
+++ src/constants/Translations/Translations.js
@@ -0,0 +1,42 @@
+import EnCommon from "./en/Common";
+import EnHome from "./en/Home";
+import EnAbout from "./en/About";
+import EnSubjects from "./en/Subjects";
+import EnHowItWorks from "./en/HowItWorks";
+import EnTeachingMethods from "./en/TeachingMethods";
+import EnATBT from "./en/ATBT";
+import EnTestimonials from "./en/Testimonials";
+import EnFAQs from "./en/FAQs";
+import EnContactUs from "./en/ContactUs";
+import EnWMCAL from "./en/WMCAL";
+import EnPricing from "./en/Pricing";
+import EnMonthlyPricing from "./en/MonthlyPricing";
+import EnBlog from "./en/Blog";
+import EnBlogDetails from "./en/BlogDetails";
+import EnTerms from "./en/Terms";
+import EnPrivacy from "./en/Privacy";
+
+const AllTranslations = {
+  en: {
+    Common: EnCommon,
+    Home: EnHome,
+    About: EnAbout,
+    Subjects: EnSubjects,
+    HowItWorks: EnHowItWorks,
+    TeachingMethods: EnTeachingMethods,
+    ATBT: EnATBT,
+    Testimonials: EnTestimonials,
+    FAQs: EnFAQs,
+    ContactUs: EnContactUs,
+    WMCAL: EnWMCAL,
+    Pricing: EnPricing,
+    MonthlyPricing: EnMonthlyPricing,
+    Blog: EnBlog,
+    BlogDetails: EnBlogDetails,
+    Terms: EnTerms,
+    Privacy: EnPrivacy
+  }
+};
+
+export default AllTranslations[process.env.REACT_APP_LANG] ||
+  AllTranslations.en;
---
+++ src/constants/Translations/en/Home.js
@@ -0,0 +1,6 @@
+export default {
+  Child247Heading:
+    "Give your child 24/7 unlimited access to expert tutors for a flat fee",
+  Child247Text:
+    "Give your child 24/7 unlimited access to expert tutors always available for a flat fee. This is the text they have given."
+};
