fde0dac04db3cf67161b47682822fd48e46ac38b
Priyadharshini Rajaram <prajaram345@gmail.com>
2020-04-23 21:48:05 +0100

    Adds Translation for Testimonials

--- src/constants/Translations/en/Testimonials.js
+++ src/constants/Translations/en/Testimonials.js
@@ -2,5 +2,9 @@ export default {
   TestIntro: {
     Title: "Our students are doing amazing. Hear it from them.",
     TestimonialsBannerAlt: "Testimonials Intro"
+  },
+  Testimonials: {
+    Title: "Still wondering if EasyA is right for your family?",
+    Action: "How we teach"
   }
 };
