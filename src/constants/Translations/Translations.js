64f2019219dd7f3dacb20b36422f429e265f5561
nikhar444 <nikhar.jain11@gmail.com>
2020-04-23 23:47:49 +0530

    HIW/Tranlations

--- src/constants/Translations/Translations.js
+++ src/constants/Translations/Translations.js
@@ -24,7 +24,7 @@ const AllTranslations = {
     About: EnAbout,
     Subjects: EnSubjects,
     HowItWorks: EnHowItWorks,
-    Ace: EnAce,
+    AceTestWithEasyA: EnAce,
     TeachingMethods: EnTeachingMethods,
     ATBT: EnATBT,
     Testimonials: EnTestimonials,
