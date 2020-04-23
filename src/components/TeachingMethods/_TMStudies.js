97e7390944aa45ee97ef8ce9b91fc5893bae423a
Praveen Kumar <praveenscience@gmail.com>
2020-04-23 21:58:39 +0100

    Subjects text refactored.

--- src/components/TeachingMethods/_TMStudies.js
+++ src/components/TeachingMethods/_TMStudies.js
@@ -2,14 +2,11 @@ import React from "react";
 import _ from "../../constants/Translations/Translations";
 
 const TMStudies = () => {
-  const { Title, Text1, Text2, Text3, Text4 } = _.TeachingMethods.TMStudies;
+  const { Title, Text } = _.TeachingMethods.TMStudies;
   return (
     <section className="TeachingMethods-Studies">
       <h3>{Title}</h3>
-      <p>{Text1}</p>
-      <p>{Text2}</p>
-      <p>{Text3}</p>
-      <p>{Text4}</p>
+      {Text && Text.map((text, key) => <p key={key}>{text}</p>)}
     </section>
   );
 };
--- src/constants/Translations/en/TeachingMethods.js
+++ src/constants/Translations/en/TeachingMethods.js
@@ -6,14 +6,12 @@ export default {
   },
   TMStudies: {
     Title: "Our Studies",
-    Text1:
+    Text: [
       "EasyA was founded on the belief that students learn better when they are given the help they need, when they need it.",
-    Text2:
       "What started out as a simple hypothesis by two former tutors (now our co-founders), based on their own teaching experience, has proven to be true.",
-    Text3:
       "We took two groups of students from leading schools across the U.K. and looked at their academic performance over the course of a year. One group was beginning of the experiment the year, the students who had EasyA were scoring 82% on average vs 78% and by the end of the year, students on EasyA had an average score of 95% compared to 82% by the end of the year.",
-    Text4:
       "More importantly, these results are being felt by the hundreds of students who are getting better 11+ and GCSE grades as the result of EasyA resulted in both groups scoring 75% and 74%. However, half way through."
+    ]
   },
   TMReviews: {
     Title: "Real EasyA reviews, real learning",
