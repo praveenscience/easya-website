c3a99e105bd906778053525da9accde62de57427
Priyadharshini Rajaram <prajaram345@gmail.com>
2020-04-23 17:27:26 +0100

    Adds TM Translation

--- src/constants/Translations/en/TeachingMethods.js
+++ src/constants/Translations/en/TeachingMethods.js
@@ -1,3 +1,19 @@
 export default {
-  EasyA: "EasyA"
+  TMIntro: {
+    Title: "EasyA's revolutionary approach to learning",
+    BasicPrinciplesTitle: "Want to get to know our tutoring team?",
+    Links: "Meet Our Tutors"
+  },
+  TMStudies: {
+    Title: "Our Studies",
+    Text1:
+      "EasyA was founded on the belief that students learn better when they are given the help they need, when they need it.",
+    Text2:
+      "What started out as a simple hypothesis by two former tutors (now our co-founders), based on their own teaching experience, has proven to be true.",
+    Text3:
+      "We took two groups of students from leading schools across the U.K. and looked at their academic performance over the course of a year. One group was beginning of the experiment the year, the students who had EasyA were scoring 82% on average vs 78% and by the end of the year, students on EasyA had an average score of 95% compared to 82% by the end of the year.",
+    Text4:
+      "More importantly, these results are being felt by the hundreds of students who are getting better 11+ and GCSE grades as the result of EasyA resulted in both groups scoring 75% and 74%. However, half way through."
+  },
+  TMReviews: {}
 };
