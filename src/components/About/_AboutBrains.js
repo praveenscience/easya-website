e9c2e980026d67f05a6fa99db22a56f91604963a
Praveen Kumar <praveenscience@gmail.com>
2020-04-23 14:21:08 +0100

    About Brains Translated.

--- src/components/About/_AboutBrains.js
+++ src/components/About/_AboutBrains.js
@@ -1,26 +1,13 @@
 import React from "react";
+import _ from "../../constants/Translations/Translations";
 
 const AboutBrains = () => {
+  const { Title, Brains } = _.About.AboutBrains;
   return (
     <section className="About-Brains">
-      <h2>Get to know the brains behind EasyA</h2>
+      <h2>{Title}</h2>
       <article className="About-Brains-Wrap">
-        {[
-          {
-            Title: "Phil, CEO & Co-founder",
-            Edu: "Cambridge University",
-            Desc:
-              "Phil graduated from Cambridge University with First Class Honours in Law. He sailed right though his degree, never achieving less than a first in his end-of-year exams. Indeed, he topped his year in many of his papers, winning the Falcon Chambers Prize, One Chancery Lane Prize, Ziegler Prize, Ginsberg Prize, Peter de Somogyi Prize, Henry Arthur Thomas Exhibition, to name a few! And Cambridge, including College, Foundation, and Squire Scholarships.",
-            Image: "https://i.imgur.com/EwN7oUK.png"
-          },
-          {
-            Title: "Dom, COO & Co-founder",
-            Edu: "Cambridge University",
-            Desc:
-              "A tutor at heart, Dom decided to leave a career in finance to focus on solving the one problem he faced as a tutor throughout university and high school: a hasslesome, inefficient and inaccessible tutoring system. Prior to co-founding EasyA, Dom worked at The Blackstone Group in New York. Prior to Blackstone, Dom worked at Goldman Sachs in its technology, media and telecom investment banking group. Dominic holds a B.S. in Finance.",
-            Image: "https://i.imgur.com/R6xbv4K.png"
-          }
-        ].map((profile, key) => (
+        {Brains.map((profile, key) => (
           <div className="About-Brains-Profile" key={key}>
             <img src={profile.Image} alt={profile.Title} />
             <h3>{profile.Title}</h3>
--- src/constants/Translations/en/About.js
+++ src/constants/Translations/en/About.js
@@ -11,7 +11,25 @@ export default {
     Text:
       "Thousands of forward thinking parents choose EasyA to empower their children. Our platform streamlines their doubts, solves their problem, and allows them to learn and grow."
   },
-  AboutBrains: {},
+  AboutBrains: {
+    Title: "Get to know the brains behind EasyA",
+    Brains: [
+      {
+        Title: "Phil, CEO & Co-founder",
+        Edu: "Cambridge University",
+        Desc:
+          "Phil graduated from Cambridge University with First Class Honours in Law. He sailed right though his degree, never achieving less than a first in his end-of-year exams. Indeed, he topped his year in many of his papers, winning the Falcon Chambers Prize, One Chancery Lane Prize, Ziegler Prize, Ginsberg Prize, Peter de Somogyi Prize, Henry Arthur Thomas Exhibition, to name a few! And Cambridge, including College, Foundation, and Squire Scholarships.",
+        Image: "https://i.imgur.com/EwN7oUK.png"
+      },
+      {
+        Title: "Dom, COO & Co-founder",
+        Edu: "Cambridge University",
+        Desc:
+          "A tutor at heart, Dom decided to leave a career in finance to focus on solving the one problem he faced as a tutor throughout university and high school: a hasslesome, inefficient and inaccessible tutoring system. Prior to co-founding EasyA, Dom worked at The Blackstone Group in New York. Prior to Blackstone, Dom worked at Goldman Sachs in its technology, media and telecom investment banking group. Dominic holds a B.S. in Finance.",
+        Image: "https://i.imgur.com/R6xbv4K.png"
+      }
+    ]
+  },
   AboutTeam: {},
   AboutCurious: {},
   AboutFeatTutors: {},
