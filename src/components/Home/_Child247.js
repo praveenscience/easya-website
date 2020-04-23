bedbc296a93b2772ec8cf1737eb8240415042536
Praveen Kumar <praveenscience@gmail.com>
2020-04-23 12:09:42 +0100

    Added translation for Child247 English.

--- src/components/Home/_Child247.js
+++ src/components/Home/_Child247.js
@@ -2,18 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import ChildExpert from "../../assets/child-expert.svg";
 import Paths from "../../constants/Paths";
+import _ from "../../constants/Translations/Translations";
 
 const Child247 = () => {
   return (
     <section className="Home-Child247">
       <div className="Home-Child247-Text">
-        <h2>
-          Give your child 24/7 unlimited access to expert tutors for a flat fee
-        </h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </p>
+        <h2>{_.Home.Child247Heading}</h2>
+        <p>{_.Home.Child247Text}</p>
         <div className="action-button">
           <Link to={Paths.HowItWorks} className="easya-btn-nav">
             How it works
