4110cbbf823390e350dcd8a4a0e720bfb9b7c80f
Praveen Kumar <praveenscience@gmail.com>
2020-04-23 14:17:19 +0100

    About Learn Translated.

--- src/components/About/_AboutLearn.js
+++ src/components/About/_AboutLearn.js
@@ -1,20 +1,18 @@
 import React from "react";
 import BackgroundBannerWithImage from "../Shared/Banners/BackgroundBannerWithImage";
 import AboutLearnAchieve from "../../assets/aboutlearnachieve.png";
+import _ from "../../constants/Translations/Translations";
 
 const AboutLearn = () => {
+  const { AboutLearn, Title, Text } = _.About.AboutLearn;
   return (
     <BackgroundBannerWithImage className="About-Learn">
       <div className="About-Learn-Image">
-        <img src={AboutLearnAchieve} alt="About Learn" />
+        <img src={AboutLearnAchieve} alt={AboutLearn} />
       </div>
       <div className="About-Learn-Text">
-        <h2>Making it easy to learn.</h2>
-        <p>
-          Thousands of forward thinking parents choose EasyA to empower their
-          children. Our platform streamlines their doubts, solves their problem,
-          and allows them to learn and grow.
-        </p>
+        <h2>{Title}</h2>
+        <p>{Text}</p>
       </div>
     </BackgroundBannerWithImage>
   );
--- src/constants/Translations/en/About.js
+++ src/constants/Translations/en/About.js
@@ -5,7 +5,12 @@ export default {
       "EasyA gives students the freedom to quickly and easily learn and solve Math problems using student-tutor matching technology. From young early math learners to veteran ,calculus solvers, EasyA makes it easy for any student to grow.",
     AboutIntroAlt: "About Intro"
   },
-  AboutLearn: {},
+  AboutLearn: {
+    AboutLearn: "About Learn",
+    Title: "Making it easy to learn.",
+    Text:
+      "Thousands of forward thinking parents choose EasyA to empower their children. Our platform streamlines their doubts, solves their problem, and allows them to learn and grow."
+  },
   AboutBrains: {},
   AboutTeam: {},
   AboutCurious: {},
